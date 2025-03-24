700569a6d5e30e719d00227eec68d28f7c63d20d
iman.dev989 <iman.dev989@gmail.com>
2025-03-24 14:23:11 +0330

    Authenticayion Code 03

--- mylms/src/app/(auth)/verify/_components/verification-form.tsx
+++ mylms/src/app/(auth)/verify/_components/verification-form.tsx
@@ -1,16 +1,31 @@
+"use client";
+
+import AuthCode from "@/app/_components/auth-code/auth-code";
+import { AuthCodeRef } from "@/app/_components/auth-code/auth-code.types";
 import { Button } from "@/app/_components/button/button";
 import Link from "next/link";
+import { useRef } from "react";
 
 const VerificationForm = () => {
+  const authCodeRef = useRef<AuthCodeRef>(null);
+
   return (
     <>
       <h5 className="text-2xl">Verification Code</h5>
       <p className="mt-2">The amazing world of programming awaits you!</p>
       <form className="flex flex-col gap-6 mt-10 flex-1">
-        <p>Auth code</p>
+        <AuthCode
+          className="mt-10"
+          ref={authCodeRef}
+          onChange={(value) => {
+            console.log(value);
+          }}
+        />
         {/* Timer placeholder - assuming you'll add a timer component here */}
         Timer
-        <Button isLink={true}>Resend Verification Code</Button>
+        <Button isLink={true} onClick={authCodeRef.current?.clear}>
+          Resend Verification Code
+        </Button>
         <Button type="submit" variant="primary">
           Confirm and Continue
         </Button>
